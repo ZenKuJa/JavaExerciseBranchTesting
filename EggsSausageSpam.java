6cfb7f93d873c4633dd11eb4fa7ecee4088ef745
zenkuja <janneskurzke@gmail.com>
2025-05-13 22:16:57 +0200

    two exerises

---
+++ EggsSausageSpam.java
@@ -0,0 +1,13 @@
+public class EggsSausageSpam{
+    public static void main(String[] args) {
+        String[] breakfast = new String[3];
+
+        breakfast[0] = "Eggs";
+        breakfast[1] = "Sausage";
+        breakfast[2] = "Spam";
+
+        for (String string : breakfast) {
+            System.out.println(string);
+        }
+    }
+}
